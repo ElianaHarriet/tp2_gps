7f3d783117cdbf1ae51ed9985b7a8447be072aef
ElianaHarriet <eharriet@fi.uba.ar>
2022-06-28 12:04:23 -0300

    Corrigiendo los nombres de las pruebas

--- src/main/test/edu/fiuba/algo3/modelo/Jugador/JugadorTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/Jugador/JugadorTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-public class PruebasJugador {
+public class JugadorTest {
 
     @Test
     public void prueba00ElJugadorSeCreaSinMovimientos() {

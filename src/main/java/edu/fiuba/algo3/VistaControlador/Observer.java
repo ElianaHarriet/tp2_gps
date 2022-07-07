486b940e9ae1c2dc33856d38561ce2486471dc32
ElianaHarriet <eharriet@fi.uba.ar>
2022-07-07 17:45:28 -0300

    AAAA

--- src/main/java/edu/fiuba/algo3/VistaControlador/Observer.java
+++
@@ -1,8 +0,0 @@
-//package edu.fiuba.algo3.VistaControlador;
-//import edu.fiuba.algo3.VistaControlador.IObserver;
-//public class Observer implements IObserver {
-//    public void Actualizar() {
-//
-//    }
-//
-//}
